e6ff805bccac185fd17b77ae7b422298884a7c75
Kartik panchal <kartikpanchal0463@gmail.com>
2024-03-20 14:36:04 +0530

    four commit

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -1,52 +1,55 @@
-import React from 'react';
+import React from "react";
 import "./Contact.css";
-import contact from "../../images/contact.jpg"
+import contact from "../../images/value.png";
 
 function Contact() {
   return (
-    <div id="contactus"> 
-    <section className='cont-wrapper'>
-      <div className='cont-container'>
-         <div className='left-side'>
-             <p>Our Contact US</p>
-             <h1>Easy to contact us</h1>
-             <span>We always ready to help by providing the best services for you. we believe a good blace to live can make you life better</span>
-             
-             <div className="main-card">
-             <div className="mini-card">
+    <div id="contactus">
+      <section className="cont-wrapper">
+        <div className="cont-container">
+          {/* this is left side */}
+          <div className="left-side">
+            <p>Our Contact US</p>
+            <h1>Easy to contact us</h1>
+            <span>
+              We always ready to help by providing the best services for you. we
+              believe a good blace to live can make you life better
+            </span>
+
+            <div className="main-card">
+              <div className="mini-card">
                 <i class="icon fa-solid fa-phone-volume"></i>
                 <span id="call">Call</span>
                 <div className="c-btn">
                   <button className="btn">call now</button>
                 </div>
-             </div>
+              </div>
 
-             <div className="mini-card">
-             <i class=" icon fa-brands fa-rocketchat"></i>
+              <div className="mini-card">
+                <i class=" icon fa-brands fa-rocketchat"></i>
                 <span id="call">Chat</span>
                 <div className="c-btn">
                   <button className="btn">chat now</button>
                 </div>
-             </div>
-             <div className="mini-card">
-             <i class="icon fa-solid fa-message"></i>
+              </div>
+
+              <div className="mini-card">
+                <i class="icon fa-solid fa-message"></i>
                 <span id="call">Message</span>
                 <div className="c-btn">
                   <button className="btn">message</button>
                 </div>
-             </div>
-             </div>
-         </div>
-        
-         <div className='right-side'>
-             <div className="ov-left">
-                <img src={contact} alt="" srcset="" width={750}/>
-             </div>
-         </div>
-      </div>
-    </section>
+              </div>
+            </div>
+          </div>
+        {/* this is right side */}
+          <div className="c-right">
+            <img className="c-img" src={contact} alt="" srcset="" />
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,112 +1,118 @@
-import React from 'react';
-import "./Residencies.css"
-import {Swiper, SwiperSlide} from "swiper/react";
+import React from "react";
+import "./Residencies.css";
 import "swiper/css";
 import r1 from "../../images/r1.png";
 import r2 from "../../images/r2.png";
-import r3 from "../../images/r3.png"
-
-
+import r3 from "../../images/r3.png";
 
 function Residencies() {
   return (
-    <div id="residencies">
-    <section className='r-wrapper'>
-      <div className='paddings innerWidth r-container'>
-        <div className='r-head flexColStart'>
-            <span className='r-title'>Best Chocies</span>
-            <span className='r-title2'>Popular Residencies</span>
+    <div className="container">
+      <div className="r-head flexColStart">
+        <span className="r-title">Best Chocies</span>
+        <span className="r-title2">Popular Residencies</span>
+      </div>
+      {/*  */}
+      <div className="cards">
+      <div className="card">
+        <img src={r1} width={250} />
+        <div className="card-body">
+          <p className="price">$ 35,853</p>
+          <h5 className="card-title">Citralan Puri Serang</h5>
+          <p className="card-text">
+            Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang,
+            Banten
+          </p>
         </div>
-      <Swiper
-        
-        spaceBetween={1}
-        slidesPerView={4}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>
-          <div className="flexColStart all-card">
-            <img src={r1} width={250}/>
-              <div className="card">
-                <p className='price'>$ 35,853</p>
-                <h2>Citralan Puri Serang</h2>
-                <p>Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten</p>
-              </div>
-          </div>
-            
-        </SwiperSlide>
-        
-        <SwiperSlide>
-            <img src={r2} width={250}/>
-            <div className="card">
-              <p className='price'>$ 47,043</p>
-              <h2>Aliva Priva Jardin</h2>
-              <p>Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta</p>
-            </div>
-        </SwiperSlide>
-        
-        <SwiperSlide>
-            <img src={r3} width={250}/>
-            <div className="card">
-              <p className='price'>$ 66,353</p>
-              <h2>Asatti Garden City</h2>
-              <p>Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat</p>
-            </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-            <img src={r2} width={250}/>
-            <div className="card">
-              <p className='price'>$ 35,853</p>
-              <h2>Citralan Puri Serang</h2>
-              <p>Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten</p>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flexColStart all-card">
-            <img src={r1} width={250}/>
-              <div className="card">
-                <p className='price'>$ 35,853</p>
-                <h2>Citralan Puri Serang</h2>
-                <p>Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten</p>
-              </div>
-          </div>
-            
-        </SwiperSlide>
-        
-        <SwiperSlide>
-            <img src={r2} width={250}/>
-            <div className="card">
-              <p className='price'>$ 47,043</p>
-              <h2>Aliva Priva Jardin</h2>
-              <p>Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta</p>
-            </div>
-        </SwiperSlide>
-        
-        <SwiperSlide>
-            <img src={r3} width={250}/>
-            <div className="card">
-              <p className='price'>$ 66,353</p>
-              <h2>Asatti Garden City</h2>
-              <p>Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat</p>
-            </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-            <img src={r2} width={250}/>
-            <div className="card">
-              <p className='price'>$ 35,853</p>
-              <h2>Citralan Puri Serang</h2>
-              <p>Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten</p>
-            </div>
-        </SwiperSlide>
-        ...
-    </Swiper>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img src={r2} width={250} />
+        <div className="card-body">
+          <p className="price">$ 47,043</p>
+          <h5 className="card-title">Aliva Priva Jardin</h5>
+          <p className="card-text">
+            Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI
+            Jakarta
+          </p>
+        </div>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img src={r3} width={250} />
+        <div className="card-body">
+          <p className="price">$ 66,353</p>
+          <h5 className="card-title">Asatti Garden City</h5>
+          <p className="card-text">
+            Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat
+          </p>
+        </div>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img src={r2} width={250} />
+        <div className="card-body">
+          <p className="price">$ 35,853</p>
+          <h5 className="card-title">Citralan Puri Serang</h5>
+          <p className="card-text">
+            Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang,
+            Banten
+          </p>
+        </div>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img src={r1} width={250} />
+        <div className="card-body">
+          <p className="price">$ 35,853</p>
+          <h5 className="card-title">Citralan Puri Serang</h5>
+          <p className="card-text">
+            Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang,
+            Banten
+          </p>
+        </div>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img src={r2} width={250} />
+        <div className="card-body">
+          <p className="price">$ 47,043</p>
+          <h5 className="card-title">Aliva Priva Jardin</h5>
+          <p className="card-text">
+            Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI
+            Jakarta
+          </p>
+        </div>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img src={r3} width={250} />
+        <div className="card-body">
+          <p className="price">$ 66,353</p>
+          <h5 className="card-title">Aliva Priva Jardin</h5>
+          <p className="card-text">
+            Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat
+          </p>
+        </div>
       </div>
 
-    </section>
+      {/*  */}
+      <div className="card">
+        <img src={r2} width={250} />
+        <div className="card-body">
+          <p className="price">$ 35,853</p>
+          <h5 className="card-title">Citralan Puri Serang</h5>
+          <p className="card-text">
+            Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang,
+            Banten
+          </p>
+        </div>
+      </div>
+      </div>
+      
+      {/* &&&&&&&&&&&&&&&&&&&& */}
     </div>
-  )
+  );
 }
 
-export default Residencies
+export default Residencies;
